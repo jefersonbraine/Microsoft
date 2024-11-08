c2f8bc7ccaa11a549d976cedcbe6af9fa04eb7b5
Jeferson <jefeleal363@gmail.com>
2024-11-08 15:00:36 -0300

    feat: exercicies chapter 3

--- learn.cs
+++ learn.cs
@@ -1,13 +1,42 @@
-Console.Writeline("Hello, World!") //escreve e no final pula a linha
+// Console.Writeline("Hello, World!") //escreve e no final pula a linha
 
-Console.Write("Escreve na mesma linha")
+// Console.Write("Escreve na mesma linha")
 
 
-string name = "Bob";
+// string name = "Bob";
 
-int number = 3;
+// int number = 3;
 
-float temperature = 34.4f;
-// Console.WriteLine("{0}\n{1}\n{2}", name, number, temperature);
+// float temperature = 34.4f;
+// // Console.WriteLine("{0}\n{1}\n{2}", name, number, temperature);
 
-Console.Write("Hello, " + name + "! " + "You have " + number + " messages in your inbox. " + "The temperature is " + temperature + " Celsius.");
+// Console.Write("Hello, " + name + "! " + "You have " + number + " messages in your inbox. " + "The temperature is " + temperature + " Celsius.");
+
+// Console.WriteLine("Hello\nWorld!");
+// Console.WriteLine("Hello\tWorld!");
+// Console.WriteLine("Hello \"World\"!");
+// Console.WriteLine("c:\\source\\repos");
+
+
+// Console.WriteLine(@"    c:\source\repos
+//             (this is where your code goes)");
+
+
+// Console.Write(@"c:\invoices");
+
+// string firstName = "Bob";
+// string message = $"Hello {firstName}!";
+// Console.WriteLine(firstName);
+
+// int version = 11;
+// string updateText = "Update to Windows";
+// string message = $"{updateText} {version}";
+
+// Console.WriteLine(message);
+
+// int version = 11;
+// string updateText = "Update to Windows";
+// Console.WriteLine($"{updateText} {version}");
+
+string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
