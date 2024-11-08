8b664ecc7cbe32db6c2ba8ce718cb95ad15e2c1f
Jeferson <jefeleal363@gmail.com>
2024-11-08 14:24:56 -0300

    feat: soluction

--- learn.cs
+++ learn.cs
@@ -3,4 +3,11 @@ Console.Writeline("Hello, World!") //escreve e no final pula a linha
 Console.Write("Escreve na mesma linha")
 
 
+string name = "Bob";
 
+int number = 3;
+
+float temperature = 34.4f;
+// Console.WriteLine("{0}\n{1}\n{2}", name, number, temperature);
+
+Console.Write("Hello, " + name + "! " + "You have " + number + " messages in your inbox. " + "The temperature is " + temperature + " Celsius.");
